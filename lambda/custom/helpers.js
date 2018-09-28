fde70411d277ffdbe3c920f0881c89a9c138b870
jared <jtruscott78@gmail.com>
2018-09-28 15:30:16 -0400

    game finished -- now to debug

--- lambda/custom/helpers.js
+++ lambda/custom/helpers.js
@@ -13,7 +13,6 @@ const specialEvent = (handlerInput, special) => {
       attributes.choices = [];
       attributes.text = passages[attributes.pid].text;
       attributes.isEnd = true;
-      console.log(JSON.stringify(attributes));
 
       break;
 
@@ -32,6 +31,7 @@ const specialEvent = (handlerInput, special) => {
         // win game and progress to next part of story
         resultText = 'You win! <break time="500ms" /> Looks like gambling comes pretty naturally to a posh mudcrab such as yourself. The creature looks both surprised and amused that you won. She rewards you with a large golden coin as promised.<audio src="soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_01"/> Seeing as a mud crab has no pockets, you decide that you should get this money put safely away. Perhaps the castle has a place you can store your coin or maybe you can find a place to hide the coin in the ocean?';
         attributes.isEnd = false;
+        attributes.inventory.item = 'coin';
         attributes.choices = [
           {
             name: 'castle',
@@ -50,7 +50,16 @@ const specialEvent = (handlerInput, special) => {
       attributes.text = `From your understanding you are about to play a dice game. You're pretty sure that an equal or higher roll means you win the coin. You, however, are a little unclear on what happens if you lose. <break time='500ms' />. The Khajitt rolls a ${computerRoll} and you roll a ${playerRoll}. ${resultText}`;
       break;
 
-      case ''
+    case 'final':
+      attributes.choices = [];
+      attributes.isEnd = true;
+
+      if (attributes.inventory.item) {
+        attributes.text = "You arrive at the castle and instantly you are overwhelmed at how huge this place is. <audio src='soundbank://soundlibrary/musical/amzn_sfx_trumpet_bugle_03'/> Like an entire sequel could be written just about your adventures here! As you wonder around the castle you see a group of mud crabs at a caffe drinking some sort of steaming brown liquid.<audio src='soundbank://soundlibrary/human/amzn_sfx_drinking_slurp_01'/> They are all wearing monocles and top hats. You greet them with a wave of your claw, which also happens to be carrying the largest coin they have ever seen. They notice that you are a posh mudcrab, and that you are, by mudcrab standards, extremely wealthy. You are immediately welcomed to their table and fitted with your own top hat and monocle. You are one of the mudcrab elite! <audio src='soundbank://soundlibrary/human/amzn_sfx_large_crowd_cheer_03'/> Tomorrow, you'll be joining the other mud crabs at their summer house, whatever that is.<audio src='https://s3.us-east-1.amazonaws.com/mudcrablife/polly.6d5ac991-ae83-455b-9e54-30eae73efb94.mp3' /> <break time='500ms' />";
+      } else {
+        attributes.text = "You arrive at the castle and instantly you are overwhelmed at how huge this place is. < audio src = 'soundbank://soundlibrary/musical/amzn_sfx_trumpet_bugle_03' /> Like an entire sequel could be written just about your adventures here! As you wonder around the castle you see a group of mud crabs at a caffe drinking some sort of steaming brown liquid.< audio src = 'soundbank://soundlibrary/human/amzn_sfx_drinking_slurp_01' /> They are all wearing monocles and top hats.You greet them with a wave of your claw.They notice that you are a posh mudcrab, but your lack of money keeps you from joining their table. < audio src = 'soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_negative_response_02' /> They motion over at the help wanted sign at the cafe.< audio src = 'soundbank://soundlibrary/foley/amzn_sfx_kitchen_ambience_01' /> You decide it's probably about time you get a job.";
+      }
+      break;
 
     // ! should never get here
     default:
@@ -77,6 +86,7 @@ const getNextPassage = (handlerInput) => {
   if (!attributes.inGame) {
     attributes.inGame = true;
     attributes.pid = 0;
+    attributes.inventory = {};
     // start the choice loops
   } else {
     const { request } = handlerInput.requestEnvelope;
